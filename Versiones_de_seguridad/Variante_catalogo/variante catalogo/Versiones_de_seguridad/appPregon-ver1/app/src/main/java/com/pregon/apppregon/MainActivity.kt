d5778db3924fd32e910c98f3e680a74d22306f00
erwin <erwinernestovaronmedina@gmail.com>
2023-06-18 12:35:52 -0500

    variante-catalogo

---
+++ Versiones_de_seguridad/Variante_catalogo/variante catalogo/Versiones_de_seguridad/appPregon-ver1/app/src/main/java/com/pregon/apppregon/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.pregon.apppregon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buton2: Button=findViewById(R.id.btn1)
+        buton2.setOnClickListener{siguiente() }
+    }
+
+    private fun siguiente(){
+    val intent:Intent = Intent(this,catalogo2::class.java)
+        startActivity(intent)
+    }
+}
+
---
+++ Versiones_de_seguridad/Variante_catalogo/variante catalogo/Versiones_de_seguridad/appPregon-ver1/app/src/main/java/com/pregon/apppregon/catalogo.kt
@@ -0,0 +1,11 @@
+package com.pregon.apppregon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class catalogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalogo)
+    }
+}
